71f1d245c5ef9881735fe2cc68f987b4d08d479b
jjub0217 <noonssub1004@gmail.com>
2025-07-17 19:57:05 +0900

    과제제출

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,20 @@
-// const a: string = "";
-// const b: string = "";
-// const c: string = ``;
+let myString: string = "Hello, TypeScript!";
+console.log("String:", myString);
 
-// let myName: string = "joo";
-// let message: string = `hello ${myName}`;
+let myNumber: number = 42;
+console.log("Number:", myNumber);
 
-// myName.toLocaleUpperCase();
-// // message = 123 // 에러
+let myBoolean: boolean = true;
+console.log("Boolean:", myBoolean);
 
-// let n: number = 100;
+let myNull: null = null;
+console.log("Null:", myNull);
 
-// // n = "100" // 에러
-// // n.toLocaleUpperCase() // 에러
+let myAny: any = "This can be anything";
+console.log("Any string:", myAny);
 
-// let count: number = 10;
-// let price: number = 9.99;
-// let tem: number = -15;
-// let distance: number = 3.4e-5;
+myAny = 10;
+console.log("changed to number:", myAny);
 
-// let total: number = count * price;
-// let avg: number = total / 2;
-
-// let numbers: number[] = [1, 2, 3, 4];
-// let firstNumber = numbers[0]; // 자동으로 number 타입으로 추론된다.
-
-// console.log(numbers); // [1,2,3,4]
-// console.log(firstNumber); // 1
-
-// let numberCount = numbers.length;
-// console.log(numberCount); // 4
-
-interface Car {
-  brand: string;
-  model: string;
-  year?: number; // 선택적 속성
-}
-
-const car1: Car = {
-  brand: "Toyota",
-  model: "Corolla",
-};
-
-const car2: Car = {
-  brand: "Tesla",
-  model: "Model S",
-  year: 2020,
-};
-
-console.log(car1, car2);
+myAny = false;
+console.log("changed to boolean:", myAny);
